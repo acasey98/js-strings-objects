9561632471f1259da00f51b458c43c7c01a7aa00
Austin Casey <thepoast@gmail.com>
2019-03-04 19:55:22 -0600

    if else ex

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('I\'m in main.js');
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+}
+
+const iAmThursty = (num) =>{
+if(num < 21){
+    return('drink water kiddo');
+} else if (num >= 21 && num <= 65){
+    return('drink some beer big boy');
+}else 
+    return('take a nap gramps');
+
+};
+
+printToDom('thirsty',iAmThursty(77));
+printToDom('thirsty',iAmThursty(55));
+printToDom('thirsty',iAmThursty(16));
